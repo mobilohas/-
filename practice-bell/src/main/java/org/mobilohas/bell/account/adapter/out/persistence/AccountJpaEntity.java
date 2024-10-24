5a535bf80fb05fd5809823af426a598678812ea7
pythonstrup <pythonstrup@gmail.com>
2024-10-24 20:54:58 +0900

    영속성 어댑터 구현

---
+++ practice-bell/src/main/java/org/mobilohas/bell/account/adapter/out/persistence/AccountJpaEntity.java
@@ -0,0 +1,21 @@
+package org.mobilohas.bell.account.adapter.out.persistence;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "account")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountJpaEntity {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+}
---
+++ practice-bell/src/main/java/org/mobilohas/bell/account/adapter/out/persistence/ActivityJpaEntity.java
@@ -0,0 +1,29 @@
+package org.mobilohas.bell.account.adapter.out.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "account")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActivityJpaEntity {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private LocalDateTime timestamp;
+  private Long ownerAccountId;
+  private Long sourceAccountId;
+  private Long targetAccountId;
+  private Long amount;
+}
